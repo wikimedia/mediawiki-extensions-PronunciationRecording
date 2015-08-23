9359f10e2b24220fec0a9d62881a8e51ac63b312
James D. Forrester <jforrester@wikimedia.org>
2015-08-22 17:07:35 -0700

    build: Add csslint to npm testing
    Change-Id: I3411741785e99202617987347ff6c9273b60623d

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-contrib-csslint' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 	grunt.loadNpmTasks( 'grunt-jscs' );
@@ -19,12 +20,15 @@ module.exports = function ( grunt ) {
 			},
 			all: [
 				'*.js',
-				'{.docs,build}/**/*.js',
-				'resources/*.js'
+				'resources/**/*.js',
+				'!resources/mediawiki.libs.recorderjs/*'
 			]
 		},
 		jscs: {
 			src: '<%= jshint.all %>'
+		},
+		csslint: {
+			src: 'resources/**/*.css'
 		},
 		jsonlint: {
 			all: 'i18n/*.json'
@@ -34,14 +38,15 @@ module.exports = function ( grunt ) {
 		},
 		watch: {
 			files: [
-				'.{jscsrc,jshintignore,jshintrc}',
+				'.{jshintignore,jshintrc,jscsrc,csslintrc}',
 				'<%= jshint.all %>',
+				'<%= csslint.all %>',
 				'<%= jsonlint.all %>'
 			],
 			tasks: 'test'
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'csslint', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
