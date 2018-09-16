169597fe6af92f15a102c37a5e0e0bceeecbc2a6
Zoranzoki21 <zorandori4444@gmail.com>
2018-09-16 22:04:28 +0000

    Add aliases for Bosnian, Croatian, Serbian
    Bug: T204436
    Change-Id: I72074d45151851a605c1343013037506b9800a56

--- PronunciationRecording.alias.php
+++ PronunciationRecording.alias.php
@@ -16,7 +16,35 @@ $specialPageAliases[ 'en' ] = [
 	'PronunciationRecording' => [ 'PronunciationRecording', 'Pronunciation Recording' ],
 ];
 
+/** Bosnian (bosanski)
+ * @author Zoranzoki21
+ */
+$specialPageAliases[ 'bs' ] = [
+	'PronunciationRecording' => [ 'SnimanjeIzgovora', 'Snimanje_izgovora' ],
+];
+
 /** Spanish (Español) */
 $specialPageAliases[ 'es' ] = [
 	'PronunciationRecording' => [ 'Grabar_pronunciación' ],
+];
+
+/** Croatian (hrvatski)
+ * @author Zoranzoki21
+ */
+$specialPageAliases[ 'hr' ] = [
+	'PronunciationRecording' => [ 'SnimanjeIzgovora', 'Snimanje_izgovora' ],
+];
+
+/** Serbian Cyrillic (српски (ћирилица))
+ * @author Zoranzoki21
+ */
+$specialPageAliases[ 'sr-ec' ] = [
+	'PronunciationRecording' => [ 'СнимањеИзговора', 'Снимање_изговора' ],
+];
+
+/** Serbian Latin (srpski (latinica))
+ * @author Zoranzoki21
+ */
+$specialPageAliases[ 'sr-el' ] = [
+	'PronunciationRecording' => [ 'SnimanjeIzgovora', 'Snimanje_izgovora' ],
 ];
